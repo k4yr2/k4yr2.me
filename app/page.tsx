20369cd62c4b034ab3ea507ece0f48e5a46dbb67
k4yr2 <kyrzkaya@gmail.com>
2025-05-24 23:12:32 +0300

    Add Typewriter effect to HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 "use client";
+import Typewriter from 'typewriter-effect';
 
 const HomePage = () => {
   return (
     <div>
       HiHa
+      <Typewriter
+        options={{
+          strings: ['Hello', 'World'],
+          autoStart: true,
+          loop: true,
+        }}
+      />
     </div>
   );
 }
