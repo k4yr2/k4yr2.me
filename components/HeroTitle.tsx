e27b3d7bb0c27ae5e5a53e4659bbfd022c3a4a40
k4yr2 <kyrzkaya@gmail.com>
2025-05-25 00:29:15 +0300

    Refactor HeroTitle component to manage Typewriter effect state and improve rendering logic

--- components/HeroTitle.tsx
+++ components/HeroTitle.tsx
@@ -1,10 +1,22 @@
 import styles from '@/styles/HeroTitle.module.css';
-import Typewriter from 'typewriter-effect';
+import Typewriter, { TypewriterClass } from 'typewriter-effect';
 import { Space_Grotesk } from 'next/font/google';
+import { useEffect, useRef, useState } from 'react';
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], weight: '700' });
 
 const HeroTitle = () => {
+    const [first, setFirst] = useState(false);
+    const secondWriter = useRef<TypewriterClass>(null);
+
+    useEffect(() => {
+        if (first && secondWriter.current) {
+            secondWriter.current
+                .typeString("M. Kayra")
+                .start();
+        }
+    }, [first]);
+
     return (
         <div className={[styles.heroTitle, spaceGrotesk.className].join(' ')}>
             <Typewriter component="div"
@@ -15,13 +27,27 @@ const HeroTitle = () => {
                 }}
                 onInit={
                     (typewriter) => {
-                        typewriter
+                        if(!first) {
+                            typewriter
                             .pauseFor(500)
                             .typeString('Hi, ').pauseFor(300)
                             .typeString("I'm Kayra").pauseFor(1000)
                             .deleteChars(7).pauseFor(300)
-                            .typeString(" M. Kayra")
+                            .callFunction(() => setFirst(true))
                             .start();
+                        }
+                    }
+                }
+            />
+            <Typewriter component="div"
+                options={{
+                delay: 50,
+                deleteSpeed: 30,
+                cursor: '',
+                }}
+                onInit={
+                    (typewriter) => {
+                        secondWriter.current = typewriter;
                     }
                 }
             />
