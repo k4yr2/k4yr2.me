ea70ffa88fc862632f6841068655bbc7e14111ae
k4yr2 <kyrzkaya@gmail.com>
2025-05-24 23:48:51 +0300

    Add Hero component to render HeroTitle

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import HeroTitle from './HeroTitle';
+
+const Hero = () => {
+    return (
+        <div style={{font: '36px'}}>
+            <HeroTitle />
+        </div>
+
+    )
+}
+
+export default Hero;
