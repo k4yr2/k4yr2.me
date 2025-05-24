7d0ded80098e874fefabc27511166d7915da532d
k4yr2 <kyrzkaya@gmail.com>
2025-05-24 14:28:06 +0300

    Fix Header component styling by correcting class name and import path

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
-import styles from '@/styles/Banner.module.css';
+import styles from '@/styles/Header.module.css';
 
 export const Header = () => {
     return (
-        <div className={styles.banner}>
+        <div className={styles.header}>
             k4yr2
         </div>
     );
