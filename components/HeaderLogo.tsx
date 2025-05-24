523951edea7de306bdde8b6248a0d8138191e4ca
k4yr2 <kyrzkaya@gmail.com>
2025-05-24 18:59:40 +0300

    Replace anchor tags with Link components in HeaderLogo and HeaderNavButton for improved navigation

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -1,16 +1,17 @@
 import styles from '@/styles/HeaderLogo.module.css';
 import { Chivo_Mono } from 'next/font/google';
+import Link from 'next/link';
 
 const rubik = Chivo_Mono({ subsets: ['latin'], weight: '700' });
 
 export const HeaderLogo = () => {
     return (
-        <a href='/' className={[styles.headerLogo, rubik.className].join(' ')}>
+        <Link href='/' className={[styles.headerLogo, rubik.className].join(' ')}>
             <span className={styles.charK}>K</span>
             <span className={styles.char4}>4</span>
             <span className={styles.charY}>Y</span>
             <span className={styles.charR}>R</span>
             <span className={styles.char2}>2</span>
-        </a>
+        </Link>
     );
 }
--- components/HeaderNavButton.tsx
+++ components/HeaderNavButton.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
+
 export const HeaderNavButton = ({ to } :  HeaderNavButtonProps) => {
     const href = `/${to}`;
     const text = to[0].toUpperCase() + to.slice(1).toLowerCase();
 
     return (
-        <a href={href}>{text}</a>
+        <Link href={href}>{text}</Link>
     );
 }
 
