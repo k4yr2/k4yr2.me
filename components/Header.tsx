b15197fa5089b6cefc66a93dd9046fe7bf9edfab
k4yr2 <kyrzkaya@gmail.com>
2025-05-24 17:38:24 +0300

    Add HeaderNav component to Header for improved navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import styles from '@/styles/Header.module.css';
 import { HeaderLogo } from './HeaderLogo';
+import HeaderNav from './HeaderNav';
 
 export const Header = () => {
     return (
         <div className={styles.header}>
             <HeaderLogo />
+            <HeaderNav />
         </div>
     );
 }
