604257153d3f4e5d24f8b0b5de81ac75dac4c563
k4yr2 <kyrzkaya@gmail.com>
2025-05-24 22:42:19 +0300

    Enhance HeaderNavButton to highlight active link with dynamic styling

--- components/HeaderNavButton.tsx
+++ components/HeaderNavButton.tsx
@@ -1,12 +1,17 @@
+"use client";
 import styles from '@/styles/HeaderNavButton.module.css';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const HeaderNavButton = ({ to } :  HeaderNavButtonProps) => {
+    const pathname = usePathname();
+    const isActive = pathname === `/${to}`;
+
     const href = `/${to}`;
     const text = to[0].toUpperCase() + to.slice(1).toLowerCase();
 
     return (
-        <Link href={href} className={styles.headerNavButton}>
+        <Link href={href} className={`${styles.headerNavButton} ${isActive ? styles.active : ''}`}>
             {text}
         </Link>
     );
