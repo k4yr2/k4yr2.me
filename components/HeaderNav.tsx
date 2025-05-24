243e087ec58b7be0a6d70f9d400787da2604098f
k4yr2 <kyrzkaya@gmail.com>
2025-05-24 18:50:54 +0300

    Refactor HeaderNav to use HeaderNavButton components for navigation links

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -1,12 +1,13 @@
 import styles from '@/styles/HeaderLogo.module.css';
+import HeaderNavButton from './HeaderNavButton';
 
 export const HeaderNav = () => {
     return (
         <nav className={styles.headerNav}>
             <div>
-                <a href="/blog" className="hover:text-gray-400">Blog</a>
-                <a href="/about" className="hover:text-gray-400">About</a>
-                <a href="/projects" className="hover:text-gray-400">Projects</a>
+                <HeaderNavButton to="home" />
+                <HeaderNavButton to="blog" />
+                <HeaderNavButton to="about" />
             </div>
         </nav>
     );
