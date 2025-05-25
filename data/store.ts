34bcab9aa44f1106cefcfd476883e88855366b1f
k4yr2 <kyrzkaya@gmail.com>
2025-05-25 21:37:42 +0300

    refactor: update calendarSource type to Activity[] in AppState interface

--- data/store.ts
+++ data/store.ts
@@ -1,17 +1,18 @@
+import { Activity } from "react-activity-calendar";
 import { create } from "zustand";
 
 export interface AppState {
     isAnimated: boolean;
     setAnimated: () => void;
-    calendarSource: object[] | null;
-    setCalendarSource: (source: object[] | null) => void;
+    calendarSource: Activity[] | null;
+    setCalendarSource: (source: Activity[] | null) => void;
 }
 
 const appStore = create<AppState>((set) => ({
     isAnimated: false,
     setAnimated: () => set(() => ({ isAnimated: true })),
     calendarSource: null,
-    setCalendarSource: (source: object[] | null) => set(() => ({ calendarSource: source })),
+    setCalendarSource: (source: Activity[] | null) => set(() => ({ calendarSource: source })),
 }))
 
 export default appStore;
