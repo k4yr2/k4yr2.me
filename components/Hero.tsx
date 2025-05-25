8d552e1276e8752d5ef7a6f92c5c71f87d93f4d9
k4yr2 <kyrzkaya@gmail.com>
2025-05-25 20:11:07 +0300

    feat: include HeroCalendar component in Hero for enhanced display

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,11 @@
+import HeroCalendar from './HeroCalendar';
 import HeroTitle from './HeroTitle';
 
 const Hero = () => {
     return (
         <div style={{font: '36px'}}>
             <HeroTitle />
+            <HeroCalendar />
         </div>
 
     )
