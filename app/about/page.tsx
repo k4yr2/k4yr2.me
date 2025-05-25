898a26dbdc0c4f656edb5908b35d0d9b8be20f37
k4yr2 <kyrzkaya@gmail.com>
2025-05-25 13:27:35 +0300

    fix: update Image component in AboutPage for better responsiveness and optimization

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 const AboutPage = () => {
   return (
     <div>
-      <Image src="/gitchart.svg" alt="Git Chart" style={{width: "100%"}}></Image>
+        <Image src="/gitchart.svg" alt="Git Chart" width={0} height={0}
+        sizes="100vw" style={{ width: '100%', height: 'auto' }}/>
     </div>
   );
 }
