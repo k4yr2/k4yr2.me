55ea8495de343886db6db1bf4c5e6fc2ba97428a
k4yr2 <kyrzkaya@gmail.com>
2025-05-25 21:34:56 +0300

    refactor: add calendarSource and setCalendarSource to AppState interface

--- data/store.ts
+++ data/store.ts
@@ -3,11 +3,15 @@ import { create } from "zustand";
 export interface AppState {
     isAnimated: boolean;
     setAnimated: () => void;
+    calendarSource: object[] | null;
+    setCalendarSource: (source: object[] | null) => void;
 }
 
 const appStore = create<AppState>((set) => ({
     isAnimated: false,
     setAnimated: () => set(() => ({ isAnimated: true })),
+    calendarSource: null,
+    setCalendarSource: (source: object[] | null) => set(() => ({ calendarSource: source })),
 }))
 
 export default appStore;
