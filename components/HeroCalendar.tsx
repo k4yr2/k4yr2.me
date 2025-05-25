b4c9dccabddcd69165331ec52e64106525bcb26d
k4yr2 <kyrzkaya@gmail.com>
2025-05-25 20:55:24 +0300

    refactor: enhance HeroCalendar layout and visibility handling

--- components/HeroCalendar.tsx
+++ components/HeroCalendar.tsx
@@ -1,9 +1,11 @@
+import k4yr2Store from "@/data/store";
 import { useState, useEffect } from "react";
 import ActivityCalendar, { ThemeInput } from "react-activity-calendar";
 
 const HeroCalendar = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const isAnimated = k4yr2Store((state) => state.isAnimated);
 
     useEffect(() => {
         fetch('/api/github-activity')
@@ -27,15 +29,20 @@ const HeroCalendar = () => {
     dark: ['#151B23', '#007728', '#02A232', '#0AC740', '#4AE168'],
     };
 
-    return <ActivityCalendar data={data} loading={loading} 
-        theme={explicitTheme} 
-        showWeekdayLabels={false}
-        hideColorLegend={true}
-        hideMonthLabels={true} 
-        blockMargin={2}
-        blockRadius={0}
-        blockSize={16}
-    />;
+    return (
+        <div className="hero-calendar" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <ActivityCalendar style={{ visibility: isAnimated ? 'visible' : 'hidden' }}
+                data={data} loading={loading}
+                theme={isAnimated ? explicitTheme : undefined}
+                showWeekdayLabels={false}
+                hideColorLegend={true}
+                hideMonthLabels={true}
+                blockMargin={2}
+                blockRadius={0}
+                blockSize={16}
+            />
+        </div>
+    )
 }
 
 export default HeroCalendar;
