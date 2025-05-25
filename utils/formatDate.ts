0d8bd7d4553e2468b6f577eb639ff69b21d96b31
k4yr2 <kyrzkaya@gmail.com>
2025-05-25 22:51:14 +0300

    feat: add formatDate utility function for date formatting

---
+++ utils/formatDate.ts
@@ -0,0 +1,15 @@
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.toLocaleString("en-US", { month: "short" });
+
+  const getOrdinal = (n: number) => {
+    const s = ["th", "st", "nd", "rd"];
+    const v = n % 100;
+    return s[(v - 20) % 10] || s[v] || s[0];
+  };
+
+  return `${month} ${day}${getOrdinal(day)}`;
+}
+
+export default formatDate;
