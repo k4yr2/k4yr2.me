1a856cf29c59a43432b8e7f9bed23567a4c08bd5
k4yr2 <kyrzkaya@gmail.com>
2025-05-25 21:16:44 +0300

    refactor: implement responsive block size adjustment in HeroCalendar

--- components/HeroCalendar.tsx
+++ components/HeroCalendar.tsx
@@ -24,6 +24,27 @@ const HeroCalendar = () => {
             });
     }, []);
 
+    const [blockSize, setBlockSize] = useState(12);
+
+    useEffect(() => {
+        const resizeHandler = () => {
+            const width = window.innerWidth;
+            
+            if (width < 500) setBlockSize(8);
+            else if (width < 768) setBlockSize(10);
+            else if (width < 1024) setBlockSize(12);
+            else if (width < 1280) setBlockSize(14);
+            else if (width < 1440) setBlockSize(16);
+            else if (width < 1920) setBlockSize(18);
+            else setBlockSize(20);
+        };
+
+        resizeHandler();
+        window.addEventListener("resize", resizeHandler);
+
+        return () => window.removeEventListener("resize", resizeHandler);
+    }, []);
+
     const explicitTheme: ThemeInput = {
     light: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
     dark: ['#151B23', '#007728', '#02A232', '#0AC740', '#4AE168'],
@@ -39,7 +60,7 @@ const HeroCalendar = () => {
                 hideMonthLabels={true}
                 blockMargin={2}
                 blockRadius={0}
-                blockSize={16}
+                blockSize={blockSize}
             />
         </div>
     )
