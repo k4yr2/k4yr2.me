92e2b126060627665f8e008e622a04f288884ec2
k4yr2 <kyrzkaya@gmail.com>
2025-05-26 15:48:53 +0300

    feat: enhance HeroTagline component to display animated tagline with Typewriter effect

--- components/HeroTagline.tsx
+++ components/HeroTagline.tsx
@@ -1,10 +1,32 @@
+import { AnimationState } from "@/data/state";
+import appStore from "@/data/store";
+import Typewriter from 'typewriter-effect';
+
 const HeroTagline = () => {
+    const animation = appStore((state) => state.home.animation.tagline);
+    const animationFrame = appStore((state) => state.home.animation.frame);
+
     return (
-        <div className="flex flex-col items-center justify-center text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
-        <p className="text-lg text-gray-600">
-            Discover amazing content and connect with our community.
-        </p>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '20px', marginTop: '20px' }}>
+            {animation == AnimationState.animated ? <>Once an anonymous builder, Now a student with real-world experience</> 
+            : animation == AnimationState.animating ? <Typewriter options={{
+                delay: 25,
+                }}
+                onInit={
+                    (typewriter) => { typewriter
+                        .pauseFor(500)
+                        .typeString('Once an anonymous builder, ').pauseFor(400)
+                        .typeString('Now a student').pauseFor(200)
+                        .typeString(' with real-world experience').pauseFor(200)
+                        .callFunction(() => {
+                            animationFrame(true);
+                        })
+                        .start();
+                    }
+                }
+            /> : null}
         </div>
     );
 }
+
+export default HeroTagline;
