a9217da205a2475bbbdd8d615e8af373fcae4450
k4yr2 <kyrzkaya@gmail.com>
2025-05-26 15:49:00 +0300

    feat: add HeroTagline component to Hero for enhanced display

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,12 @@
 import HeroCalendar from './HeroCalendar';
+import HeroTagline from './HeroTagline';
 import HeroTitle from './HeroTitle';
 
 const Hero = () => {
     return (
         <div style={{font: '36px'}}>
             <HeroTitle />
+            <HeroTagline />
             <HeroCalendar />
         </div>
 
